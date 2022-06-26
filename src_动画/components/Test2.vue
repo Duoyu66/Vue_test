14feb08ec7a143ae6a784c29b701544c48acf30e
libingle <2523890936@qq.com>
2022-06-26 14:20:59 +0800

    6.26.3

---
+++ src_动画/components/Test2.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <button @click="isShow=!isShow">显示/隐藏</button>
+<transition-group  :appear="true">
+  <h1  key="1" v-show="isShow">你好啊</h1>
+  <h1 key='2' v-show="isShow">你好啊</h1>
+
+</transition-group>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data(){
+    return {
+      isShow:true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  background-color: yellow;
+}
+.v-enter-active{
+  animation: atguigu 1s;
+}
+.v-leave-active{
+  animation: atguigu 1s reverse;
+}
+@keyframes atguigu {
+  from{
+    transform: translateX(-100%);
+  }
+  to{
+    transform: translateX(0);
+  }
+
+}
+</style>
