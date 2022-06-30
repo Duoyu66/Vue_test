c409de01291e1b58a8234f445a614ab3c8a92560
libingle <2523890936@qq.com>
2022-06-30 16:01:21 +0800

    测试能否打开！！

---
+++ vue_test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="row">
+      <Banner/>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+<!--          <a class="list-group-item" href="./about.html">About</a>-->
+<!--          <a class="list-group-item active" href="./home.html">Home</a>-->
+          <router-link    :replace="true" class="list-group-item" active-class="active" to="{name:'guanyu'}">About</router-link>
+          <router-link  :replace="true" class="list-group-item" active-class="active" to="/home">Home</router-link>
+//城北参透我，是我社情期许过，人身上又来出却只剩归途，我从笔去从容
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <router-view ></router-view>
+<!--            此处到底站是什么组件,得看用户电机的是哪个导航项-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Banner from "./components/Banner";
+	export default {
+		name:'App',
+components:{Banner}
+  }
+</script>
---
+++ vue_test/src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  beforeDestroy() {
+    console.log('Home组件即将被销毁了')
+  },
+  /*  mounted() {
+      console.log('Home子组件挂在完毕了')
+      window.homeRouter=this.$route
+      window.homeRouter=this.$router
+    }*/
+}
+</script>
+
+<style scoped>
+
+</style>
